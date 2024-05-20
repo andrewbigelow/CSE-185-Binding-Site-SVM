3af803f16017f7c13c062e2b4f76e84cfe7f87d8
andrewbigelow <abigelow@ucsd.edu>
2024-05-20 13:50:09 -0700

    added comment

--- binding_site_svm.py
+++ binding_site_svm.py
@@ -30,3 +30,7 @@ y_pred = clf.predict(X_test)
 # Evaluate the model
 print("Accuracy:", accuracy_score(y_test, y_pred))
 print("Classification Report:\n", classification_report(y_test, y_pred))
+
+
+
+# TESTING COMMIT
