8a2b523119a965f3d3fa7d859c5f18f3d924f488
andrewbigelow <abigelow@ucsd.edu>
2024-05-24 16:51:11 -0700

    adding file so it can be considered a package

--- mVarScan/file_parsing.py
+++ mVarScan/file_parsing.py
@@ -9,5 +9,12 @@ def main():
     parser.add_argument("bam", help="Indexed BAM files", type=str)
     parser.add_argument("-o", "--out", help="Write output to file. ", metavar="FILE", type=str, required=False)
     parser.add_argument("-f", "--fasta-ref", help="indexed fasta reference genome file", type=str, metavar="FILE", required=False)
+    
+    parser.add_argument("-m", "--min-var-frequency", \
+                        help="minumum frequency to call a non-reference a mutation. If not called: will auto to 0.2", \
+                        type=float, required=False)
+    parser.add_argument("-h", "--min-freq-for-hom", \
+                        help="minumum frequency to call a non-reference a homozygous mutation. If not called: will auto to 0.8", \
+                        type=float, required=False)
 
     args = parser.parse_args()
