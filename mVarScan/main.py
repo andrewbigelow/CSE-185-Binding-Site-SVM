c6b97c7e0b6c773962a19612a2d24fc28811f586
NumaanFormoli <numanformoliz@gmail.com>
2024-05-24 18:25:51 -0700

    Fixed parser

--- mVarScan/main.py
+++ mVarScan/main.py
@@ -1,7 +1,7 @@
 from mVarScan import MpileupParser, VariantCaller, FileParsing
 import argparse
 
-def main() :
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog = "mVarScan",
         description = "Command-line python package to perform variant calling on sam or bam files"
@@ -10,7 +10,6 @@ def main() :
     parser.add_argument("mpileup", help="mpileup file", type=str, metavar="FILE")
     parser.add_argument("-o", "--out", help="Write output to file. ", metavar="FILE", type=str, required=False)
     parser.add_argument("-f", "--fasta-ref", help="indexed fasta reference genome file", type=str, metavar="FILE", required=False)
-    
     parser.add_argument("-m", "--min-var-frequency", \
                         help="minumum frequency to call a non-reference a mutation. If not called: will auto to 0.2", \
                         type=float, required=False)
@@ -29,6 +28,7 @@ def main() :
     min_frequency_for_hom = 0.8
     if args.min_frequency_for_hom != None :
         min_frequency_for_hom = args.min_frequency_for_hom
-    parser = MpileupParser("path/to/mpileup")
-    caller = VariantCaller(parser)
+
+    mpileup_parser = MpileupParser(parser.mpileup)
+    caller = VariantCaller(mpileup_parser)
     caller.process()
