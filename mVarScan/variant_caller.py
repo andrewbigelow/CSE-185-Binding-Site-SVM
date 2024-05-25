a349b6a8c81d997728f8ac1ea4be3db13bbc2e5f
andrewbigelow <abigelow@ucsd.edu>
2024-05-24 17:43:18 -0700

    Merge branch 'main' of https://github.com/andrewbigelow/CSE-185-mVarScan

--- mVarScan/variant_caller.py
+++ mVarScan/variant_caller.py
@@ -1,4 +1,5 @@
 from mpileup_parser import MpileupParser
+from file_parsing import args
 
 def is_SNP(counts, total_reads, min_var_freq) :
     for base, count in counts.items() :
@@ -14,3 +15,16 @@ def is_homozygous_nonreference_SNP(base, freq, min_homozygous_freq) :
     return False, None, 0
 
 def count_bases(self, read_bases):
+    return
+
+
+def main() :
+    # Populate variables from command line tool
+    mpileup = args.mpileup
+    min_var_frequency = 0.2
+    if args.min_var_frequency != None :
+        min_var_frequency = args.min_var_frequency
+    
+    min_frequency_for_hom = 0.8
+    if args.min_frequency_for_hom != None :
+        min_frequency_for_hom = args.min_frequency_for_hom
