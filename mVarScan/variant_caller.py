42b50766f2ec2678f901c35a69920c84373bc80b
NumaanFormoli <numanformoliz@gmail.com>
2024-05-24 17:40:34 -0700

    implemented count_bases

--- mVarScan/variant_caller.py
+++ mVarScan/variant_caller.py
@@ -1,6 +1,15 @@
 from mpileup_parser import MpileupParser
 
 def is_SNP(count, ):
-
+    print("hello")
 
 def count_bases(self, read_bases):
+    counts = {'A': 0, 'C': 0, 'G': 0, 'T': 0, 'N': 0, 'del': 0}
+    for base in read_bases:
+        if(base in counts):
+            counts[base] += 1
+        elif(base.upper() in counts):
+            counts[base.upper()] += 1
+        elif(base == '*' | base == '-'):
+            counts['del'] += 1
+    return counts
