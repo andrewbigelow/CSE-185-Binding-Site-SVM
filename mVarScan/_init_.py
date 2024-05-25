de4b1fa1b847017c0a5d906823024a76c3dc8bdf
NumaanFormoli <numanformoliz@gmail.com>
2024-05-24 18:20:33 -0700

    Changed the main.py

--- mVarScan/_init_.py
+++ mVarScan/_init_.py
@@ -1,2 +1,4 @@
 from .mpileup_parser import MpileupParser
 from .variant_caller import VariantCaller
+from .file_parsing import FileParsing
+
--- mVarScan/main.py
+++ mVarScan/main.py
@@ -1,6 +1,25 @@
-from mVarScan import MpileupParser, VariantCaller
+from mVarScan import MpileupParser, VariantCaller, FileParsing
+import argparse
 
 def main() :
+    parser = argparse.ArgumentParser(
+        prog = "mVarScan",
+        description = "Command-line python package to perform variant calling on sam or bam files"
+    )
+
+    parser.add_argument("mpileup", help="mpileup file", type=str, metavar="FILE")
+    parser.add_argument("-o", "--out", help="Write output to file. ", metavar="FILE", type=str, required=False)
+    parser.add_argument("-f", "--fasta-ref", help="indexed fasta reference genome file", type=str, metavar="FILE", required=False)
+    
+    parser.add_argument("-m", "--min-var-frequency", \
+                        help="minumum frequency to call a non-reference a mutation. If not called: will auto to 0.2", \
+                        type=float, required=False)
+    parser.add_argument("-h", "--min-freq-for-hom", \
+                        help="minumum frequency to call a non-reference a homozygous mutation. If not called: will auto to 0.8", \
+                        type=float, required=False)
+
+    args = parser.parse_args()
+    
     # Populate variables from command line tool
     mpileup = args.mpileup
     min_var_frequency = 0.2
