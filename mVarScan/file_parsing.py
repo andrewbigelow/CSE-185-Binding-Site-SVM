580a75a917f66709fe5072b068de4191e2d02a56
andrewbigelow <abigelow@ucsd.edu>
2024-05-24 17:21:24 -0700

    added two simple functions as well as fixed parser argument to take mpileup instead of sam/bam

--- mVarScan/file_parsing.py
+++ mVarScan/file_parsing.py
@@ -6,7 +6,7 @@ def main():
         description = "Command-line python package to perform variant calling on sam or bam files"
     )
 
-    parser.add_argument("bam", help="Indexed BAM files", type=str)
+    parser.add_argument("mpileup", help="mpileup file", type=str, metavar="FILE")
     parser.add_argument("-o", "--out", help="Write output to file. ", metavar="FILE", type=str, required=False)
     parser.add_argument("-f", "--fasta-ref", help="indexed fasta reference genome file", type=str, metavar="FILE", required=False)
     
--- mVarScan/variant_caller.py
+++ mVarScan/variant_caller.py
@@ -1,2 +1,12 @@
-def is_SNP(count, ) :
-    
+def is_SNP(counts, total_reads, min_var_freq) :
+    for base, count in counts.items() :
+        if base != 'N' and base != 'del' :
+            freq = count / total_reads
+            if freq > min_var_freq :
+                return True, base, freq
+    return False, None, 0
+
+def is_homozygous_nonreference_SNP(base, freq, min_homozygous_freq) :
+    if freq > min_homozygous_freq :
+        return True, base, freq
+    return False, None, 0
