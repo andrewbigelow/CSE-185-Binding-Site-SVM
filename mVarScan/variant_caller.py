6b230f6f4b40a657d8bee77408c16320624460f4
NumaanFormoli <numanformoliz@gmail.com>
2024-05-29 13:37:37 -0700

    Made some changes

--- mVarScan/variant_caller.py
+++ mVarScan/variant_caller.py
@@ -20,8 +20,8 @@ class VariantCaller:
 
 
     # Call if is_SNP() returns true
-    def is_homozygous_nonreference_SNP(base, freq, min_homozygous_freq) :
-        if freq > min_homozygous_freq :
+    def is_homozygous_nonreference_SNP(self, base, freq) :
+        if freq > self.min_frequency_for_hom :
             return True, base, freq
         return False, None, 0
 
@@ -52,9 +52,9 @@ class VariantCaller:
                 total_count += counts[count]
         
         table = [[ref_count, alt_count], [total_count - ref_count, total_count - alt_count]]
-        _, p_value = fisher_exact(table)
+        odds_ratio, p_value = fisher_exact(table)
 
-        return p_value
+        return odds_ratio, p_value
     
     # TODO: Check if total reads is correct
     def find_snps(self):
@@ -66,7 +66,7 @@ class VariantCaller:
                 total_reads = sum(counts.values())
                 is_variant, variant_base, freq = self.is_SNP(counts, total_reads)
                 if is_variant:
-                    pval = self.get_pval(counts, ref_base)
+                    odds_ratio, pval = self.get_pval(counts, ref_base)
                     is_homo, homo_base, homo_freq = self.is_homozygous_nonreference_SNP(counts, total_reads)
                     if (is_homo and pval < self.pvalue) :
                         print(f"Homozygous SNP found at {chrom}:{pos} -> {ref_base} to {variant_base} with frequency {freq:.2f} and p value {pval}")
